78fef0e66e4ce911b3d81bb43f3a467dcf2b9a73
AfshanGit <afshan.nazneen@gmail.com>
2024-02-14 12:36:06 +0530

    Update helloworld.js

--- blocks/helloworld/helloworld.js
+++ blocks/helloworld/helloworld.js
@@ -1,7 +1,7 @@
 /**
  *
  * @param {HTMLElement} block
- */
+ 
 async function queryDemo() {
   const offset = 0;
   const limit = 20;
@@ -28,4 +28,30 @@ export default async function decorate(block) {
   });
   block.append(table);
 
+}
+ */
+
+import ffetch from '../../scripts/ffetch.js';
+
+async function queryDemo() {
+    const limit = 20;
+    const entries = ffetch('/query-index.json').sheet('default').limit(limit);
+    const table = document.createElement('table');
+    for await (const post of entries) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `<td>${post.title}</td><td>${post.description}</td>`;
+        table.append(tr);
+    }
+    return table;
+}
+
+export default async function decorate(block) {
+    const table = await queryDemo();
+    [...block.children].forEach((row) => {
+        const image = row.getElementsByTagName('img')[0];
+        row.innerHTML = `<div><img src = ${image.src}></img><br/><p>${row.innerText}</p></div>`;
+        
+    });
+    block.append(table);
+  
 }
